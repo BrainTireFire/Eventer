7f52a1071d121a0bcc2e5c4116c9a4e296330d5b
Maciej Kawęcki <skimiskim12@gmail.com>
2022-01-24 23:45:08 +0100

    client side error handling and validation

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -26,7 +22,7 @@ namespace API.Controllers
 
             if (result.IsSuccess && result.Value != null)
             {
-                return Ok(result);
+                return Ok(result.Value);
             }
 
             if (result.IsSuccess && result.Value == null)
