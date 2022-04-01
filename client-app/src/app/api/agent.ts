ccfc6600c30e094bd249b54a1597066974910f11
Maciej Kawęcki <skimiskim12@gmail.com>
2022-04-01 14:17:42 +0200

    user profile client side v1
    user profile client side v1

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { Activity, ActivityFormValues } from '../models/activity';
+import { Profile } from '../models/profile';
 import { User, UserFormValues } from '../models/user';
 import { store } from '../stores/store';
 
@@ -80,6 +81,10 @@ const Activities = {
     attend: (id: string) => requests.post<void>(`/activities/${id}/attend`, {}),
 }
 
+const Profiles = {
+    get: (username: string) => requests.get<Profile>(`/profiles/${username}`),
+}
+
 const Account = {
     current: () => requests.get<User>('/account'),
     login: (user: UserFormValues) => requests.post<User>('/account/login', user),
@@ -88,7 +93,8 @@ const Account = {
 
 const agent = {
     Activities,
-    Account
+    Account,
+    Profiles
 }
 
 export default agent;
